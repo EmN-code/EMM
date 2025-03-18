7f944acfeeceed06854f2fb875939efd553e65ff
EmN-code <wangh@emnets.cn>
2025-03-18 15:33:46 +0800

    correct

--- benchmark/RIOT/mqtt-sn/main.c
+++ benchmark/RIOT/mqtt-sn/main.c
@@ -1,186 +1,161 @@
 #include <stdio.h>
 #include <string.h>
-#include <stdbool.h>
-#include "timex.h"
-#include "ztimer.h"
-#include "shell.h"
+#include <stdlib.h>
+#include <arpa/inet.h>
+#include "msg.h"
+#include "net/emcute.h"
 #include "thread.h"
-#include "mutex.h"
-#include "paho_mqtt.h"
-#include "MQTTClient.h"
-#include "periph/gpio.h" 
+#include <xtimer.h>
 
-#define THREAD_STACKSIZE        (THREAD_STACKSIZE_IDLE)
-
-#define LED_GPIO GPIO12
-#define BUF_SIZE                        1024
-#define MQTT_VERSION_v311               4       /* MQTT v3.1.1 version is 4 */
-#define COMMAND_TIMEOUT_MS              4000
-
-
-/**
- * @brief Default MQTT port
- */
-#define DEFAULT_MQTT_PORT               1883
-
-/**
- * @brief Keepalive timeout in seconds
- */
-#define DEFAULT_KEEPALIVE_SEC           10
-
-#ifndef MAX_LEN_TOPIC
-#define MAX_LEN_TOPIC                   100
+#ifndef EMCUTE_ID
+#define EMCUTE_ID           ("gertrud")
 #endif
+#define EMCUTE_PRIO         (THREAD_PRIORITY_MAIN - 1)
 
-#ifndef MAX_TOPICS
-#define MAX_TOPICS                      4
-#endif
+#define NUMOFSUBS           (16U)
+#define TOPIC_MAXLEN        (64U)
+#define EMQX_SERVER_IP "47.102.103.1"  
+static char stack[THREAD_STACKSIZE_DEFAULT];
+static msg_t queue[8];
 
-#define IS_CLEAN_SESSION                1
-#define IS_RETAINED_MSG                 0
+static emcute_sub_t subscriptions[NUMOFSUBS];
+static char topics[NUMOFSUBS][TOPIC_MAXLEN];
 
-static MQTTClient client;
-static Network network;
-
-char* DEFAULT_MQTT_CLIENT_ID = "esp32_test";
-char* DEFAULT_MQTT_USER = "esp32";
-char* DEFAULT_MQTT_PWD = "esp32";
-char* DEFAULT_MQTT_IP = "47.102.103.1";
-char* DEFAULT_TOPIC = "v1/devices/me/telemetry";
-
-int mqtt_discon(void)
+static void *emcute_thread(void *arg)
 {
-
-    int res = MQTTDisconnect(&client);
-    if (res < 0) {
-        printf("mqtt_example: Unable to disconnect\n");
-    }
-    else {
-        printf("mqtt_example: Disconnect successful\n");
-    }
-
-    NetworkDisconnect(&network);
-    return res;
+    (void)arg;
+    emcute_run(CONFIG_EMCUTE_DEFAULT_PORT, EMCUTE_ID);
+    return NULL;    
 }
 
-int mqtt_connect(void)
+static void on_pub(const emcute_topic_t *topic, void *data, size_t len)
 {
+    char *in = (char *)data;
 
-    char *remote_ip = (char*)DEFAULT_MQTT_IP;
-
-    int ret = -1;
-
-    /* ensure client isn't connected in case of a new connection */
-    if (client.isconnected) {
-        printf("mqtt_example: client already connected, disconnecting it\n");
-        MQTTDisconnect(&client);
-        NetworkDisconnect(&network);
+    printf("### got publication for topic '%s' [%i] ###\n",
+           topic->name, (int)topic->id);
+    for (size_t i = 0; i < len; i++) {
+        printf("%c", in[i]);
     }
-
-    int port = DEFAULT_MQTT_PORT;
-    MQTTPacket_connectData data = MQTTPacket_connectData_initializer;
-    data.MQTTVersion = MQTT_VERSION_v311;
-
-    data.clientID.cstring = (char *)DEFAULT_MQTT_CLIENT_ID;
-    data.username.cstring = (char *)DEFAULT_MQTT_USER;
-    data.password.cstring = (char *)DEFAULT_MQTT_PWD;
-    data.keepAliveInterval = DEFAULT_KEEPALIVE_SEC;
-    data.cleansession = IS_CLEAN_SESSION;
-    data.willFlag = 0;
-
-    printf("mqtt_example: Connecting to MQTT Broker from %s %d\n",
-            remote_ip, port);
-    printf("mqtt_example: Trying to connect to %s, port: %d\n",
-            remote_ip, port);
-
-    ret = NetworkConnect(&network, remote_ip, port);
-    if (ret < 0) {
-        printf("mqtt_example: Unable to connect\n");
-        return ret;
-    }
-
-    printf("user:%s clientId:%s password:%s\n", data.username.cstring,
-             data.clientID.cstring, data.password.cstring);
-    ret = MQTTConnect(&client, &data);
-    if (ret < 0) {
-        printf("mqtt_example: Unable to connect client %d\n", ret);
-        return ret;
-    }
-    else {
-        printf("mqtt_example: Connection successfully\n");
-    }
-
-    return (ret > 0) ? 0 : 1;
+    puts("");
 }
 
-int mqtt_pub(int temp)
+static int connect_to_broker(const char *addr, uint16_t port, const char *will_topic, const char *will_message)
 {
-    enum QoS qos = QOS0;
+    sock_udp_ep_t gw = { .family = AF_INET, .port = port };
 
-    MQTTMessage message;
-    message.qos = qos;
-    message.retained = IS_RETAINED_MSG;
-    char json[200];  
-    sprintf(json, "{temperature:%d}", temp);
-    printf("[Send] Message:%s\n", json);
-
-    message.payload = json;
-    message.payloadlen = strlen((char *)message.payload);
-
-    int rc;
-    if ((rc = MQTTPublish(&client, DEFAULT_TOPIC, &message)) < 0) {
-        printf("mqtt_example: Unable to publish (%d)\n", rc);
-    }
-    else {
-        printf("mqtt_example: Message (%s) has been published to topic %s"
-               "with QOS %d\n",
-               (char *)message.payload, DEFAULT_TOPIC, (int)message.qos);
-    }
-
-    return rc;
-}
-
-
-
-
-void *_alarm(void *arg){
-    (void) arg;
-    int led_state = 1;
-    for (int i = 0; i < 10 * 2; ++i)
-    {
-        led_state = !led_state;
-        gpio_write(LED_GPIO, led_state);
-        ztimer_sleep(ZTIMER_MSEC, 1 * MS_PER_SEC);
-    }
-    return NULL;
-}
-
-static unsigned char buf[BUF_SIZE];
-static unsigned char readbuf[BUF_SIZE];
-
-int main(void)
-{
-
-    gpio_init(LED_GPIO, GPIO_OUT);
-    NetworkInit(&network);
-    MQTTClientInit(&client, &network, COMMAND_TIMEOUT_MS, buf, BUF_SIZE,
-                   readbuf, BUF_SIZE);
-    MQTTStartTask(&client);
-    ztimer_sleep(ZTIMER_MSEC, 6 * MS_PER_SEC);
-    char stack[THREAD_STACKSIZE];
-    thread_create(stack, sizeof(stack), THREAD_PRIORITY_MAIN - 1,
-                            THREAD_CREATE_STACKTEST, _alarm, NULL, "alarm");
-
-    while(1){
-
-        if (mqtt_connect()) {
-            int temp = 22.7;
-            mqtt_pub(temp);
-            ztimer_sleep(ZTIMER_MSEC, 5 * MS_PER_SEC);
-            mqtt_discon();
+    int result = inet_pton(AF_INET, EMQX_SERVER_IP, &gw.addr.ipv4);
+    if (result <= 0) {
+        if (result == 0) {
+            puts("Error: invalid IPv4 address format");
+        } else {
+            perror("inet_pton");
         }
     }
 
+    size_t len = will_message ? strlen(will_message) : 0;
+    if (emcute_con(&gw, true, will_topic, will_message, len, 0) != EMCUTE_OK) {
+        printf("-----");
+        printf("error: unable to connect to [%s]:%i\n", addr, (int)port);
+        return 1;
+    }
+    
+    printf("Successfully connected to gateway at [%s]:%i\n", addr, (int)port);
+
+    return 0;
+}
+
+static int disconnect_from_broker(void)
+{
+    int res = emcute_discon();
+    if (res == EMCUTE_NOGW) {
+        puts("error: not connected to any broker");
+        return 1;
+    }
+    else if (res != EMCUTE_OK) {
+        puts("error: unable to disconnect");
+        return 1;
+    }
+    puts("Disconnect successful");
+    return 0;
+}
+
+static int publish_to_topic(const char *topic_name, const char *data, unsigned qos)
+{
+    emcute_topic_t t;
+    unsigned flags = qos;
+
+    t.name = topic_name;
+    if (emcute_reg(&t) != EMCUTE_OK) {
+        puts("error: unable to obtain topic ID");
+        return 1;
+    }
+
+    if (emcute_pub(&t, data, strlen(data), flags) != EMCUTE_OK) {
+        printf("error: unable to publish data to topic '%s [%i]'\n",
+                t.name, (int)t.id);
+        return 1;
+    }
+
+    printf("Published %" PRIuSIZE " bytes to topic '%s [%i]'\n",
+           strlen(data), t.name, t.id);
+
+    return 0;
+}
+
+static int subscribe_to_topic(const char *topic_name, unsigned qos)
+{
+    unsigned flags = qos;
+
+    if (strlen(topic_name) > TOPIC_MAXLEN) {
+        puts("error: topic name exceeds maximum possible size");
+        return 1;
+    }
+
+    unsigned i = 0;
+    for (; (i < NUMOFSUBS) && (subscriptions[i].topic.id != 0); i++) {}
+    if (i == NUMOFSUBS) {
+        puts("error: no memory to store new subscriptions");
+        return 1;
+    }
+
+    subscriptions[i].cb = on_pub;
+    strcpy(topics[i], topic_name);
+    subscriptions[i].topic.name = topics[i];
+    if (emcute_sub(&subscriptions[i], flags) != EMCUTE_OK) {
+        printf("error: unable to subscribe to %s\n", topic_name);
+        return 1;
+    }
+
+    printf("Now subscribed to %s\n", topic_name);
+    return 0;
+}
+
+int main(void)
+{
+    puts("MQTT-SN example application\n");
+    msg_init_queue(queue, ARRAY_SIZE(queue));
+    memset(subscriptions, 0, (NUMOFSUBS * sizeof(emcute_sub_t)));
+    thread_create(stack, sizeof(stack), EMCUTE_PRIO, 0, emcute_thread, NULL, "emcute");
+
+    if (connect_to_broker("47.102.103.1", CONFIG_EMCUTE_DEFAULT_PORT, NULL, NULL) != 0) {
+        return 1;
+    }
+
+    if (subscribe_to_topic("test/topic", EMCUTE_QOS_0) != 0) {
+        return 1;
+    }
+
+    while (1) {
+        if (publish_to_topic("test/topic", "Hello, world!", EMCUTE_QOS_0) != 0) {
+            puts("Error publishing message");
+        }
+        xtimer_sleep(5); 
+    }
+
+    if (disconnect_from_broker() != 0) {
+        return 1;
+    }
 
     return 0;
 }
